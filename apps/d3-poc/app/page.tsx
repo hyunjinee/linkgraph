80ba3c67865b0ff43c41beea06aae0d51c4bb2fe
hyunjin <leehj0110@kakao.com>
2023-05-16 16:59:22 +0900

    chore: docs command 추가

--- apps/d3-poc/app/page.tsx
+++ apps/d3-poc/app/page.tsx
@@ -1,12 +1,29 @@
+import { select } from 'd3';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Home() {
   const [data, setData] = useState([20, 30, 45, 70, 26]);
+  const svgRef = useRef();
+
+  useEffect(() => {
+    if (!svgRef.current) {
+      return;
+    }
+
+    const svg = select(svgRef.current);
+
+    svg
+      .selectAll('circle')
+      .data(data)
+      .join((enter) => enter.append('circle'));
+  }, [data]);
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24 bg-red-100">
       <span>hdi</span>
+
+      <svg ref={svgRef}></svg>
     </main>
   );
 }
