072f3cfc11cebe667168900c68c16f18edabce82
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2023-05-20 15:04:02 +0900

    chore: add prisma client / connect to planetscale (#5)
    * chore: add prisma client
    
    * feat: connection, get Product data 테스트 완료
    
    * chore: remove package lock
    
    * chore: add prisma generate command

---
+++ apps/frontend/app/api/product/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server';
+import prisma from '~/prisma/prisma';
+
+export const GET = async () => {
+  const data = await prisma.product.findMany();
+  return NextResponse.json(data);
+};
