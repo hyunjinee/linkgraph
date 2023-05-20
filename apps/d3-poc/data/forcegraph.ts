81851b263657507703bd2aa24843b09d94e6c4e0
hyunjin <leehj0110@kakao.com>
2023-05-20 15:11:39 +0900

    chore: change data source, target

--- apps/d3-poc/data/forcegraph.ts
+++ apps/d3-poc/data/forcegraph.ts
@@ -1,5 +1,5 @@
-export const nodes = [{ id: 'Alice' }, { id: 'Bob' }, { id: 'Carol' }];
+export const nodes = [{ id: 'Arts Web' }, { id: 'Communitiy Vision' }, { id: 'Silicon Valley Creates' }];
 export const links = [
-  { source: 0, target: 1 }, // Alice → Bob
-  { source: 1, target: 2 }, // Bob → Carol
+  { source: 0, target: 1 }, // Arts Web → Communitiy Vision
+  { source: 0, target: 2 }, // Arts Web → Silicon Valley Creates
 ];
