797cd75ea3aae00fe9d5fb7bce2c5473bba1c005
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2023-05-21 23:30:29 +0900

    feat: D3 Force Graph 고도화 작업 (#6)
    * docs: done 항목 추가
    
    * chore: force graph data 분리
    
    * chore: change data source, target
    
    * feat: add text to force graph
    
    * feat: alignment baseline, text-anchor
    
    * feat: add multiple nodes
    
    * chore: add leaf node size
    
    * chore: node의 크기 상수화

--- apps/d3-poc/app/graph/page.tsx
+++ apps/d3-poc/app/graph/page.tsx
@@ -1,14 +1,10 @@
 'use client';
+
 import { NextPage } from 'next';
 import { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
-const nodes = [{ id: 'Alice' }, { id: 'Bob' }, { id: 'Carol' }];
-
-const links = [
-  { source: 0, target: 1 }, // Alice → Bob
-  { source: 1, target: 2 }, // Bob → Carol
-];
+import { nodes, links, MANY_BODY_STRENGTH } from '~/data/forcegraph';
 
 const centerX = 960 / 2;
 const centerY = 500 / 2;
@@ -19,17 +15,37 @@ const Graph: NextPage = () => {
   useEffect(() => {
     const simulation = d3
       .forceSimulation(nodes as any)
-      .force('charge', d3.forceManyBody())
-      .force('link', d3.forceLink(links))
+      .force('charge', d3.forceManyBody().strength(MANY_BODY_STRENGTH))
+      .force(
+        'link',
+        d3.forceLink(links).distance((link) => link.distance),
+      )
       .force('center', d3.forceCenter(centerX, centerY));
 
     const svg = d3.select(svgRef.current);
-    const circles = svg.selectAll('circle').data(nodes).enter().append('circle').attr('r', 10);
+    const circles = svg
+      .selectAll('circle')
+      .data(nodes)
+      .enter()
+      .append('circle')
+      .attr('r', (node: any) => node.size)
+      .attr('fill', 'grey');
+    const text = svg
+      .selectAll('text')
+      .data(nodes)
+      .enter()
+      .append('text')
+      .text((node: any) => node.id)
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'middle');
+
     const lines = svg.selectAll('line').data(links).enter().append('line').attr('stroke', 'black');
 
     simulation.on('tick', () => {
       circles.attr('cx', (node: any) => node.x);
       circles.attr('cy', (node: any) => node.y);
+
+      text.attr('x', (node: any) => node.x).attr('y', (node: any) => node.y);
 
       lines
         .attr('x1', (link: any) => link.source.x)
