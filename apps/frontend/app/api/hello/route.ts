2df1f632f199631ec295fc6640e7a5f4b7737c5a
hyunjin <leehj0110@kakao.com>
2023-05-22 00:30:44 +0900

    feat: hello router

---
+++ apps/frontend/app/api/hello/route.ts
@@ -0,0 +1,13 @@
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+import { authOptions } from '~/api/auth/[...nextauth]/route';
+
+export const GET = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ message: 'You are not logged in' }, { status: 401 });
+  }
+
+  return NextResponse.json({ name: session.user?.name });
+};
--- apps/frontend/app/api/product/route.ts
+++ apps/frontend/app/api/product/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '~/prisma/prisma';
+import prisma from '../../../prisma/prisma';
 
 export const GET = async () => {
   const data = await prisma.product.findMany();
