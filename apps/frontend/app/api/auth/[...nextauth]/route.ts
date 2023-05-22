0340da22fafa3e1af033c614d71479ea7365c591
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2023-05-23 00:46:20 +0900

    feat: production 환경에서 구글 로그인 (#12)
    * chore: add Link Model
    
    * chore: add kakaoprovider

--- apps/frontend/app/api/auth/[...nextauth]/route.ts
+++ apps/frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth, { AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import KakaoProvider from 'next-auth/providers/kakao';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 
@@ -11,6 +12,10 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID as string,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET as string,
     }),
   ],
   pages: {
