88dddada9bbe1f8925734976bced08fc81b363cf
hyunjin <leehj0110@kakao.com>
2023-05-23 00:39:22 +0900

    chore: add kakaoprovider

--- apps/frontend/app/signin/page.tsx
+++ apps/frontend/app/signin/page.tsx
@@ -1,26 +1,28 @@
 'use client';
 import type { NextPage } from 'next';
-import { signOut, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import GoogleSignInButton from '~/components/GoogleSignInButton';
 
 const SignInPage: NextPage = () => {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
 
-  console.log(session);
+  // console.log(session);
 
-  if (session) {
-    return (
-      <div>
-        로그인 되었습니다.
-        <button onClick={() => signOut()}>로그아웃</button>
-      </div>
-    );
-  }
+  // if (session) {
+  //   return (
+  //     <div>
+  //       로그인 되었습니다.
+  //       <button onClick={() => signOut()}>로그아웃</button>
+  //     </div>
+  //   );
+  // }
 
   return (
     <section>
       <button>구글로 로그인</button>
       <GoogleSignInButton />
+
+      <button onClick={() => signIn('kakao')}>카카오로 로그인</button>
     </section>
   );
 };
