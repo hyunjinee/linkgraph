a8b545e420dd3d227980f816a756754b3a917fb6
hyunjin <leehj0110@kakao.com>
2023-05-23 21:07:16 +0900

    chore: add signinpage

--- apps/auth-practice/app/signin/page.tsx
+++ apps/auth-practice/app/signin/page.tsx
@@ -1,3 +1,16 @@
 export default function SignInPage() {
-  return <section>hi</section>;
+  return (
+    <section className="flex">
+      <div>
+        <div>
+          <h1>Sign in to your account</h1>
+        </div>
+        <div>
+          <form>
+            <div></div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
