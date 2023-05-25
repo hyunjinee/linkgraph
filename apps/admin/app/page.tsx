f468097d6982ca5a0bdf9877050b07a74d94dda1
hyunjin <leehj0110@kakao.com>
2023-05-25 17:16:37 +0900

    feat: prisma 분리

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,9 +1,16 @@
+import { Title, Text, Card } from '@tremor/react';
 import type { NextPage } from 'next';
+import UserTable from './components/UserTable';
 
 const Home: NextPage = () => {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="p-4 mx-auto md:p-10 max-w-7xl">
+      <Title>Users</Title>
+      <Text>A list of users retrieved from a MySQL database (PlanetScale).</Text>
+
+      <Card className="mt-6">
+        <UserTable />
+      </Card>
     </main>
   );
 };
--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+
 const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
 
