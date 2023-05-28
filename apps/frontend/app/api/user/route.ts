2ad7132f3e2812a9d168551b3584407cd9717826
hyunjin <leehj0110@kakao.com>
2023-05-28 17:41:52 +0900

    chore: product 예제 스키마 제거

--- apps/frontend/app/api/user/route.ts
+++ apps/frontend/app/api/user/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server';
 import prisma from '../../../prisma/prisma';
 
 export const GET = async () => {
-  const data = await prisma.product.findMany();
+  const data = await prisma.user.findMany();
   return NextResponse.json(data);
 };
