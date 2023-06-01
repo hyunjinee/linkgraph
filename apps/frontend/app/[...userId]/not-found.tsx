a68ad557907775082e3cc73c134ff34657ea95d6
hyunjin <leehj0110@kakao.com>
2023-06-01 14:00:29 +0900

    feat: add not-found page

---
+++ apps/frontend/app/[...userId]/not-found.tsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <h1>없어요.</h1>;
+};
+
+export default NotFound;
--- apps/frontend/app/[...userId]/page.tsx
+++ apps/frontend/app/[...userId]/page.tsx
@@ -1,7 +1,22 @@
 import { NextPage } from 'next';
+import { notFound } from 'next/navigation';
+import { prisma } from '@linkgraph/db';
 
-const User: NextPage<{ params: { userId: string } }> = ({ params }) => {
-  console.log(params.userId);
+const User = async ({ params }: { params: { userId: string[] } }) => {
+  console.log(params);
+
+  const userId = params.userId[0];
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+  console.log(user);
+
+  // if (true) {
+  //   notFound();
+  // }
 
   return <div>hi</div>;
 };
