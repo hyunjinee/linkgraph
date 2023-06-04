9d21b65d3b9cc05c544e2ec395065a435fb49d3e
hyunjin <leehj0110@kakao.com>
2023-06-04 17:37:26 +0900

    test: 클라우드 front 배포 테스트

--- apps/frontend/app/temp/page.tsx
+++ apps/frontend/app/temp/page.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import GoogleSignInButton from '~/components/GoogleSignInButton';
 
@@ -12,9 +13,13 @@ const Temp: NextPage = () => {
       <br />
 
       <Link href="/image-upload">프로필 이미지 업로드</Link>
-      <Link href="/image-upload">프로필 이미지 업로드</Link>
-      <Link href="/image-upload">프로필 이미지 업로드</Link>
-      <Link href="/image-upload">프로필 이미지 업로드</Link>
+
+      <Image
+        src="https://d1wdpf820bqp3t.cloudfront.net/%E1%84%80%E1%85%B5%E1%86%BA%E1%84%92%E1%85%A5%E1%84%87%E1%85%AE%E1%86%BC.jpg"
+        width={200}
+        height={200}
+        alt="hi"
+      />
     </div>
   );
 };
