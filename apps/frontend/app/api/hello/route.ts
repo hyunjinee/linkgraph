568d55d2ee0b900c8c8e59d046485e795e05b2f4
hyunjin <leehj0110@kakao.com>
2023-06-05 17:31:59 +0900

    feat: add Next Swagger

--- apps/frontend/app/api/hello/route.ts
+++ apps/frontend/app/api/hello/route.ts
@@ -2,6 +2,15 @@ import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 import { authOptions } from '~/api/auth/[...nextauth]/route';
 
+/**
+ * @swagger
+ * /api/hello:
+ *   get:
+ *     description: Returns the hello world
+ *     responses:
+ *       200:
+ *         description: Hello World!
+ */
 export const GET = async () => {
   const session = await getServerSession(authOptions);
   console.log('hi');
