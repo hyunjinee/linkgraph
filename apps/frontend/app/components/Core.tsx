be84c92d500cb30eadfca6c494c6c336b4dcf30b
hyunjin <leehj0110@kakao.com>
2023-06-25 13:27:18 +0900

    refactor: AuthContext 사용

--- apps/frontend/app/components/Core.tsx
+++ apps/frontend/app/components/Core.tsx
@@ -4,17 +4,23 @@ import type { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
+import React from 'react';
+import { Session } from 'next-auth';
 
 const client = new QueryClient();
 
-const Core: React.FC<PropsWithChildren> = ({ children }) => {
+export const AuthContext = React.createContext<Session | null>(null);
+
+const Core = ({ children, session }: PropsWithChildren<{ session: Session | null }>) => {
   return (
-    <SessionProvider>
-      <QueryClientProvider client={client}>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </SessionProvider>
+    <AuthContext.Provider value={session}>
+      <SessionProvider>
+        <QueryClientProvider client={client}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </SessionProvider>
+    </AuthContext.Provider>
   );
 };
 
