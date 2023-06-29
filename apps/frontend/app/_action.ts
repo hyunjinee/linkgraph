31e92260925f1955e5b567e502cc3246e92e7d6f
hyunjin <leehj0110@kakao.com>
2023-06-29 11:56:09 +0900

    docs: server action 주석 추가

--- apps/frontend/app/_action.ts
+++ apps/frontend/app/_action.ts
@@ -1,5 +1,12 @@
 'use server';
 
+/*
+  this is server action file.
+  this project is not using server actions now.
+
+  프로젝트에 서버 액션을 추가하려면 이 파일을 수정한다.
+*/
+
 import { createLink } from '@linkgraph/db';
 import { revalidatePath } from 'next/cache';
 
