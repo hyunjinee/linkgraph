1be05168ae2f9c5040a9cf35039cdd9291baf21b
hyunjin <leehj0110@kakao.com>
2023-07-01 16:01:02 +0900

    feat: fetch user, link count in main

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,48 +1,43 @@
-import { getServerSession } from 'next-auth';
 import Image from 'next/image';
 import Link from 'next/link';
-import CountUp from 'react-countup';
-
-import { countLink } from '@linkgraph/db';
-import { authOptions } from '~/api/auth/[...nextauth]/route';
+import prisma from '@linkgraph/db';
 
 /* ISR. Incremental Static Regeneration
   
    유저수, 연결된 링크 수를 5분마다 갱신한다.
 */
 const Home = async () => {
-  const session = await getServerSession(authOptions);
-  const count = await countLink(session?.user.id);
+  const [userCount, linkCount] = await Promise.all([prisma.user.count(), prisma.link.count()]);
 
   return (
-    <main className="flex items-center justify-between w-full h-full px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <section className="flex flex-col justify-center h-full gap-3 shrink-0">
+    <main className="mx-auto flex h-full w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+      <section className="flex h-full shrink-0 flex-col justify-center gap-3">
         <div className="text-6xl font-bold ">
           나를 소개하는
           <br /> 링크를 연결해보세요!
         </div>
         <button
           type="button"
-          className="inline-flex justify-center w-32 px-4 py-2 mt-4 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+          className="mt-4 inline-flex w-32 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
         >
           <Link href="/profile">그래프 만들기</Link>
         </button>
       </section>
-      <section className="flex flex-col items-center justify-center w-full h-full">
+      <section className="flex h-full w-full flex-col items-center justify-center">
         <div className="flex gap-5">
           <div className=" h-[35rem] w-[21rem] rounded-2xl bg-gray-50 shadow-lg">
             <Image className="mx-auto pt-28" src="/connect.png" width={60} height={60} alt="연결" />
 
-            <div className="flex items-center justify-center mt-10 text-lg font-semibold">
-              140,293명이
+            <div className="mt-10 flex items-center justify-center text-lg font-semibold">
+              {userCount}명이
               <br /> 가입했습니다.
             </div>
           </div>
           <div className=" h-[35rem] w-[21rem] rounded-2xl bg-gray-50 shadow-lg">
             <Image className="mx-auto pt-28" src="/connect.png" width={60} height={60} alt="연결" />
             {/* <CountUp end={140293} separator="," /> -> 클라이언트 컴포넌트에서 동작 */}
-            <div className="flex items-center justify-center mt-10 text-lg font-semibold">
-              123,123개의
+            <div className="mt-10 flex items-center justify-center text-lg font-semibold">
+              {linkCount}개의
               <br /> 링크를 연결합니다.
             </div>
           </div>
