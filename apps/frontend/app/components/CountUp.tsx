541004391afe35912517f2c5ad0fac67e89db035
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2023-07-03 11:51:44 +0900

    style: main page 반응형 디자인 적용 (#59)
    * style: main page 반응형
    
    * style: NavBar 반응형 적용

--- apps/frontend/app/components/CountUp.tsx
+++ apps/frontend/app/components/CountUp.tsx
@@ -4,10 +4,11 @@ import ReactCountUp from 'react-countup';
 
 type CountUpProps = {
   number: number;
+  className: string;
 };
 
-const CountUp = ({ number }: CountUpProps) => {
-  return <ReactCountUp end={number} separator=","></ReactCountUp>;
+const CountUp = ({ number, className }: CountUpProps) => {
+  return <ReactCountUp end={number} separator="," className={className}></ReactCountUp>;
 };
 
 export default CountUp;
--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -22,38 +22,35 @@ const Home = async () => {
   const { userCount, linkCount } = await getStatistics();
 
   return (
-    <main className="relative flex flex-col flex-wrap w-full h-full px-4 mx-auto max-w-7xl sm:justify-center sm:px-6 lg:px-8">
-      {/* Left */}
-
-      <div className="relative flex flex-col w-full sm:flex-row">
-        <section className="flex flex-col h-full gap-3 shrink-0 sm:justify-center">
-          <div className="mt-10 text-5xl font-bold sm:mt-0 sm:text-6xl">
+    <main className="mx-auto h-full w-full max-w-7xl">
+      <section className="mt-8 grid grid-cols-1 px-8 md:grid-cols-2 lg:mt-24">
+        <div className="mb-12 md:mb-0 lg:mt-24">
+          <h1 className="mt-10 text-5xl font-bold sm:mt-0 sm:text-6xl">
             나를 소개하는
             <br /> 링크를 연결해보세요!
-          </div>
+          </h1>
+          <p>프로필 페이지에서 나를 소개하는 링크를 생성하면 그래프 형태로 보여집니다.</p>
           <button
             type="button"
-            className="inline-flex justify-center w-32 px-4 py-2 mt-4 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+            className="mt-4 inline-flex w-32 justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
           >
             <Link href="/profile">그래프 만들기</Link>
           </button>
-        </section>
-
-        <section className="ml-32 flex w-[700px] shrink-0 justify-center">
+        </div>
+        <div className="flex w-full items-center justify-center">
           <Image src="linkgraph.svg" width={300} height={300} alt="temp" />
-        </section>
-      </div>
+        </div>
+      </section>
 
-      <section className="mt-10">
-        <h2 className="text-xl font-semibold">
-          <CountUp number={userCount} />
-          명의 유저가
-          <CountUp number={linkCount} />
-          개의 링크를 연결하고 있습니다.
+      <section className="mt-32 flex items-center justify-center">
+        <h2 className="text-xl font-semibold ">
+          <CountUp number={userCount} className="text-3xl text-pink-500" />
+          <span className="text-pink-500">명</span>의 유저가{' '}
+          <CountUp number={linkCount} className="text-3xl text-sky-500" />
+          <span className="text-sky-500">개</span>의 링크를 연결하고 있습니다.
         </h2>
       </section>
 
-      {/* Right */}
       {/* <section className="flex flex-col items-center justify-center w-full h-full">
         <div className="flex gap-5">
           <div className=" h-[35rem] w-[21rem] rounded-2xl bg-gray-50 shadow-lg">
