a9bcd310168451014a99ae2ad2262c3bc33318b5
hyunjin <leehj0110@kakao.com>
2023-07-09 15:33:48 +0900

    chore: font (body)

---
+++ apps/service/app/components/LinkUploadForm.tsx
@@ -0,0 +1,5 @@
+const LinkUploadForm = () => {
+  return <div>LinkUploadForm</div>;
+};
+
+export default LinkUploadForm;
--- apps/service/app/profile/page.tsx
+++ apps/service/app/profile/page.tsx
@@ -11,7 +11,7 @@ import LinkList from '~/components/LinkList';
 
 const ProfilePage: NextPage = () => {
   return (
-    <main className="mx-auto h-full w-full max-w-7xl p-4 md:p-10">
+    <main className="w-full h-full p-4 mx-auto max-w-7xl md:p-10">
       <div>
         <Profile />
 
@@ -21,24 +21,29 @@ const ProfilePage: NextPage = () => {
           <LinkList />
         </div>
 
-        <section className="w-1/2 rounded-xl p-4 shadow-md">
+        <div className="w-1/2 h-32 bg-red-50">
+          <h3>링크 업로드</h3>
+          <Image src="/icons/clip.svg" width={48} height={48} alt="link" />
+        </div>
+
+        {/* <section className="w-1/2 p-4 shadow-md rounded-xl">
           <h2>링크 업로드</h2>
-          <div className="flex w-full items-center justify-center gap-4">
-            <div className="flex h-24 w-24 shrink-0 items-center justify-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+          <div className="flex items-center justify-center w-full gap-4">
+            <div className="flex items-center justify-center w-24 h-24 rounded-full shrink-0 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
               <Image src="/icons/palette.svg" width={48} height={48} alt="palette" />
             </div>
-            <div className="flex h-24 w-24 shrink-0 items-center justify-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+            <div className="flex items-center justify-center w-24 h-24 rounded-full shrink-0 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
               <Image src="/icons/clip.svg" width={48} height={48} alt="palette" />
             </div>
-            <div className="flex h-24 w-24 shrink-0 items-center justify-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+            <div className="flex items-center justify-center w-24 h-24 rounded-full shrink-0 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
               <Image src="/icons/palette.svg" width={48} height={48} alt="palette" />
             </div>
-            <div className="flex h-24 w-24 shrink-0 items-center justify-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+            <div className="flex items-center justify-center w-24 h-24 rounded-full shrink-0 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
               <Image src="/icons/palette.svg" width={48} height={48} alt="palette" />
             </div>
           </div>
           <div>이미지를 선택해주세요.</div>
-        </section>
+        </section> */}
       </div>
     </main>
   );
