9ad827ee0d75152c11e3a96e2d5d01e224033d00
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2023-07-09 13:56:22 +0900

    feat: 사용자는 자신의 URL을 설정할 수 있다. (#61)
    * style: profile page Image 등록 부분 반응형 적용 (#60)
    
    * style: update layout in profile page
    
    * style: profile 페이지 맨 윗부분 레이아웃
    
    * feat: add delete mutation to button
    
    * chore: remove console
    
    * feat: update  User Schema (add User description)
    
    * docs: update readme
    
    * chore: mutation wrapping을 어떻게 해야할지 고민이요~
    
    * feat: 유저는 URL을 설정할 수 있다.

--- apps/frontend/app/profile/page.tsx
+++ apps/frontend/app/profile/page.tsx
@@ -2,55 +2,43 @@
 
 import type { NextPage } from 'next';
 import Image from 'next/image';
-import { useQuery } from '@tanstack/react-query';
 
 import LinkForm from '~/components/LinkForm';
 import Profile from '~/components/Profile';
-import ProfileURL from '~/components/ProfileURL';
 import LinkList from '~/components/LinkList';
 
 // TODO if not logged in -> redirect to homepage
 
 const ProfilePage: NextPage = () => {
-  // change to ssr
-  const { data: links } = useQuery<Link[]>({
-    queryKey: ['links'],
-    queryFn: async () => {
-      const res = await fetch('/api/link');
-      return res.json();
-    },
-    suspense: true,
-  });
-
   return (
     <main className="mx-auto h-full w-full max-w-7xl p-4 md:p-10">
       <div>
         <Profile />
 
         {/* <ProfileURL /> */}
-        {/* <div className="flex gap-4">
+        <div className="flex gap-4">
           <LinkForm />
-          <LinkList links={links} />
+          <LinkList />
         </div>
 
-        <section className="w-1/2 p-4 shadow-md rounded-xl">
+        <section className="w-1/2 rounded-xl p-4 shadow-md">
           <h2>링크 업로드</h2>
-          <div className="flex items-center justify-center w-full gap-4">
-            <div className="flex items-center justify-center w-24 h-24 rounded-full shrink-0 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+          <div className="flex w-full items-center justify-center gap-4">
+            <div className="flex h-24 w-24 shrink-0 items-center justify-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
               <Image src="/icons/palette.svg" width={48} height={48} alt="palette" />
             </div>
-            <div className="flex items-center justify-center w-24 h-24 rounded-full shrink-0 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+            <div className="flex h-24 w-24 shrink-0 items-center justify-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
               <Image src="/icons/clip.svg" width={48} height={48} alt="palette" />
             </div>
-            <div className="flex items-center justify-center w-24 h-24 rounded-full shrink-0 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+            <div className="flex h-24 w-24 shrink-0 items-center justify-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
               <Image src="/icons/palette.svg" width={48} height={48} alt="palette" />
             </div>
-            <div className="flex items-center justify-center w-24 h-24 rounded-full shrink-0 bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+            <div className="flex h-24 w-24 shrink-0 items-center justify-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
               <Image src="/icons/palette.svg" width={48} height={48} alt="palette" />
             </div>
           </div>
           <div>이미지를 선택해주세요.</div>
-        </section> */}
+        </section>
       </div>
     </main>
   );
