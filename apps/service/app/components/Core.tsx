55894c89a8136f744d93e91f96ee77ac523763f8
hyunjin <leehj0110@kakao.com>
2023-07-10 11:27:22 +0900

    refactor: 컴포넌트 import 방식

--- apps/service/app/components/Core.tsx
+++ apps/service/app/components/Core.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import type { PropsWithChildren } from 'react';
+import React, { type PropsWithChildren } from 'react';
+import type { Session } from 'next-auth';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
-import React from 'react';
-import { Session } from 'next-auth';
 
 const client = new QueryClient();
 
---
+++ apps/service/app/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Core } from './Core';
+export { default as Navbar } from './Navbar';
+export { default as Search } from './Search';
+export { default as CountUp } from './CountUp';
