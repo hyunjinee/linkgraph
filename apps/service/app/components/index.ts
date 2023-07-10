d125cd1d5f5f364f8c0620975e262facc0105af3
hyunjin <leehj0110@kakao.com>
2023-07-10 12:25:08 +0900

    refactor: 컴포넌트 export 정리

--- apps/service/app/components/index.ts
+++ apps/service/app/components/index.ts
@@ -2,3 +2,6 @@ export { default as Core } from './Core';
 export { default as Navbar } from './Navbar';
 export { default as Search } from './Search';
 export { default as CountUp } from './CountUp';
+export { default as LinkUploadForm } from './LinkUploadForm';
+export { default as Profile } from './Profile';
+export { default as LinkList } from './LinkList';
