23c0d25da17ee19753f6e24ff33e44a33194d31c
hyunjin <leehj0110@kakao.com>
2023-07-12 14:36:16 +0900

    feat: user가 설정한 url로도 검색 가능

--- apps/service/app/[userId]/page.tsx
+++ apps/service/app/[userId]/page.tsx
@@ -2,24 +2,43 @@ import { notFound } from 'next/navigation';
 import prisma from '@linkgraph/db';
 
 import GraphTest from '~/components/Graph';
-import LinkGraph from '~/components/LinkGraph';
 
 const Graph = async ({ params: { userId } }: { params: { userId: string } }) => {
-  const user = await prisma.user.findUnique({
+  // 유저의 Id로 넘어오는 값이 uuid 일수도 있지만 유저가 설정한 URL이 올 수도 있다.
+  // 이를 구분하기 위해 먼저 uuid로 유저를 찾고, 없으면 url로 유저를 찾는다.
+  let isUUID = true;
+
+  let user = await prisma.user.findUnique({
     where: {
       id: userId,
     },
   });
+
+  if (!user) {
+    isUUID = false;
+    user = await prisma.user.findUnique({
+      where: {
+        url: userId,
+      },
+    });
+  }
+
   // 해당 유저를 찾을 수 없으면 not-found 페이지로 리다이렉션
   if (!user) {
     notFound();
   }
 
-  const userLinks = (await prisma.link.findMany({
-    where: {
-      userId,
-    },
-  })) as Link[];
+  const userLinks = isUUID
+    ? ((await prisma.link.findMany({
+        where: {
+          userId,
+        },
+      })) as Link[])
+    : ((await prisma.link.findMany({
+        where: {
+          userId: user.id,
+        },
+      })) as Link[]);
 
   const nodes: any = [{ id: userId, img: user.image, size: 60 }];
   const links = userLinks.map((link) => ({ source: userId, target: link.id }));
