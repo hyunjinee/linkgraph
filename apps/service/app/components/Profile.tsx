d3dc5543353257826a223e9adf225c921fc65044
hyunjin <leehj0110@kakao.com>
2023-07-12 11:48:36 +0900

    feat: 유저 정보 수정

--- apps/service/app/components/Profile.tsx
+++ apps/service/app/components/Profile.tsx
@@ -9,29 +9,19 @@ import { cloudFrontURL } from '@linkgraph/site-info';
 import { getCurrentDateTime } from '@linkgraph/utils';
 
 import { useUpload } from '~/hooks/useUpload';
-import { useDeleteProfileImage } from '~/queries/profile';
+import { useDeleteProfileImage, useUpdateProfile } from '~/queries/profile';
 
 const Profile = () => {
-  const [imageBlobURL, setImageBlobURL] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
   const [userURL, setUserURL] = useState('');
+  const [imageBlobURL, setImageBlobURL] = useState('');
+  const [isEditMode, setIsEditMode] = useState(false);
 
   const { data: session, update } = useSession();
   const [file, upload] = useUpload();
   const { deleteProfileImage } = useDeleteProfileImage();
-
-  const { mutate } = useMutation({
-    mutationFn: async () => {
-      const res = await fetch('/api/profile-url', {
-        method: 'PATCH',
-        body: JSON.stringify({
-          userId: session?.user.id,
-          url: userURL,
-        }),
-      });
-
-      return res.json();
-    },
-  });
+  const { updateProfile } = useUpdateProfile();
 
   const handleImageUpload = async () => {
     const image = await upload();
@@ -93,16 +83,14 @@ const Profile = () => {
   };
 
   const handleImageDelete = async () => {
-    await Promise.all([
-      deleteProfileImage(session?.user.id),
-      update({
-        ...session,
-        user: {
-          ...session?.user,
-          profileImage: '',
-        },
-      }),
-    ]);
+    await deleteProfileImage(session?.user.id);
+    await update({
+      ...session,
+      user: {
+        ...session?.user,
+        profileImage: '',
+      },
+    });
     setImageBlobURL('');
     toast.success('이미지 삭제에 성공했습니다.');
   };
@@ -146,26 +134,75 @@ const Profile = () => {
       <div className="h-[2px] w-60 bg-gray-100 sm:h-60 sm:w-[2px]" />
 
       {/* 소개 */}
-      <div className="self-start mt-2">
-        <div className="text-3xl font-semibold">{session?.user.name}님 안녕하세요.😊</div>
-
-        {/* 이부분 수정 */}
-        <div className="mt-4">
-          <div>이름: {session?.user.name}</div>
-          <div>{session?.user.description}</div>
-          <div>{session?.user.url}</div>
-          <button onClick={() => mutate()}>수정</button>
-        </div>
-
-        <input
-          type="text"
-          // defaultValue={session?.user.id}
-          value={userURL}
-          onChange={(e) => setUserURL(e.target.value)}
-          className="w-[400px] border-2 border-pink-500"
-        />
-
-        <div>https://link-graph.vercel.app/{session?.user.url ?? session?.user.id}</div>
+      <div className="self-start w-full mt-2">
+        {isEditMode ? (
+          <div className="flex flex-col gap-4">
+            <input
+              type="text"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="이름"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              // value={linkTitle}
+              // onChange={(e) => setLinkTitle(e.target.value)}
+            />
+            <input
+              type="text"
+              id="linkTitle"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="한 줄 소개"
+              required
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <input
+              type="text"
+              id="linkTitle"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="URL"
+              required
+              value={userURL}
+              onChange={(e) => setUserURL(e.target.value)}
+            />
+            <button
+              onClick={async () => {
+                await updateProfile({
+                  userId: session?.user.id,
+                  name,
+                  description,
+                  url: userURL,
+                });
+                setIsEditMode(false);
+              }}
+              className="inline-flex justify-center w-32 px-4 py-2 mt-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+            >
+              저장
+            </button>
+          </div>
+        ) : (
+          <>
+            <div className="text-3xl font-semibold">{session?.user.name}님 안녕하세요.😊</div>
+            <div className="mt-4">
+              <div>{session?.user.description}</div>
+              <div>https://link-graph.vercel.app/{session?.user.url ?? session?.user.id}</div>
+            </div>
+            <button
+              className="inline-flex justify-center w-32 px-4 py-2 mt-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              onClick={async () => {
+                setIsEditMode(true);
+                await updateProfile({
+                  userId: session?.user.id,
+                  name,
+                  description,
+                  url: userURL,
+                });
+              }}
+            >
+              수정
+            </button>
+          </>
+        )}
       </div>
     </section>
   );
