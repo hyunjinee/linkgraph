b58f1698272527c95cb69f1a73e4c0e16a9cacbd
hyunjin <leehj0110@kakao.com>
2023-07-12 14:15:19 +0900

    style: add sample linkgraph image

--- apps/service/app/page.tsx
+++ apps/service/app/page.tsx
@@ -23,13 +23,13 @@ const Home = async () => {
 
   return (
     <main className="w-full h-full mx-auto max-w-7xl">
-      <section className="grid grid-cols-1 px-8 mt-8 md:grid-cols-2 lg:mt-24">
+      <section className="grid grid-cols-1 px-10 mt-8 md:grid-cols-2 md:mt-24 ">
         <div className="mb-12 md:mb-0 lg:mt-24">
           <h1 className="mt-10 text-5xl font-bold sm:mt-0 sm:text-6xl">
             나를 소개하는
             <br /> 링크를 연결해보세요!
           </h1>
-          <p>프로필 페이지에서 나를 소개하는 링크를 생성하면 그래프 형태로 보여집니다.</p>
+          <p className="mt-2">프로필 페이지에서 나를 소개하는 링크를 생성하면 그래프 형태로 보여집니다.</p>
           <button
             type="button"
             className="inline-flex justify-center w-32 px-4 py-2 mt-4 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
@@ -38,7 +38,7 @@ const Home = async () => {
           </button>
         </div>
         <div className="flex items-center justify-center w-full">
-          <Image src="linkgraph.svg" width={300} height={300} alt="temp" />
+          <Image src="/linkgraph.png" width={500} height={400} alt="linkgraph" />
         </div>
       </section>
 
@@ -50,9 +50,6 @@ const Home = async () => {
           <span className="text-sky-500">개</span>의 링크를 연결하고 있습니다.
         </h2>
       </section>
-
-      {/* TODO delete this */}
-      <h1 className="w-full text-3xl font-bold text-center text-purple-400">서비스 준비중입니다.</h1>
     </main>
   );
 };
