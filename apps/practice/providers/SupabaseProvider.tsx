436f422d546078f0b708e99fa979a01f1153506f
hyunjin <leehj0110@kakao.com>
2023-07-19 20:44:31 +0900

    feat: supabase provider

--- apps/practice/providers/SupabaseProvider.tsx
+++ apps/practice/providers/SupabaseProvider.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import { Database } from '@/types_db';
-
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 type SupabaseProviderProps = {
   children: React.ReactNode;
 };
 
-const supabaseClient = 1
+const supabaseClient = createClientComponentClient();
 
 export default function SupabaseProvider({ children }: SupabaseProviderProps) {
-
-  return <div>SupabaseProvider</div>;
+  return <SessionContextProvider supabaseClient={supabaseClient}>SupabaseProvider</SessionContextProvider>;
 }
