11f2fb7fa25c6a102510cf8892a6f48ab27a7653
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2023-07-21 00:13:34 +0900

    chore: add simple skeleton component (#78)

--- apps/service/src/app/profile/page.tsx
+++ apps/service/src/app/profile/page.tsx
@@ -1,7 +1,9 @@
 import { getServerSession } from 'next-auth';
 
 import { LinkUploadForm, Profile, LinkList } from '~/components';
-import { authOptions } from '../api/auth/[...nextauth]/route';
+import { authOptions } from '~/app/api/auth/[...nextauth]/route';
+import { Suspense } from 'react';
+import LinkListSkeleton from '~/components/LinkListSkeleton';
 
 const ProfilePage = async () => {
   const session = await getServerSession(authOptions);
@@ -10,13 +12,18 @@ const ProfilePage = async () => {
     return <div>로그인이 필요합니다.</div>;
   }
 
+  const userId = session.user.id;
+
   return (
     <main className="w-full h-full p-4 mx-auto max-w-7xl md:p-10">
       <Profile />
       <div className="lg:flex">
         <LinkUploadForm />
         <div className="mb-4 " />
-        <LinkList />
+
+        <Suspense fallback={<LinkListSkeleton />}>
+          <LinkList userId={userId} />
+        </Suspense>
       </div>
     </main>
   );
