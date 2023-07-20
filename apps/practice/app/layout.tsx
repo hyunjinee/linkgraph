293ae5f404bbafe52e47943c3695953cbc9f2cd7
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2023-07-20 12:56:32 +0900

    chore: Learn Supabase ! (#76)
    * chore: tailwind practice
    
    * style: add transition when hover search
    
    * chore: make some tailwind component
    
    * chore: refactor search
    
    * chore: learn supabase
    
    * chore: supabase client
    
    * feat: supabase provider
    
    * chore: add some util packages to login
    
    * chore: add some util packages to login

--- apps/practice/app/layout.tsx
+++ apps/practice/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next';
 import { Figtree } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import Sidebar from '@/components/Sidebar';
+import SupabaseProvider from '@/providers/SupabaseProvider';
+
 
 const font = Figtree({ subsets: ['latin'] });
 
@@ -16,8 +18,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" suppressHydrationWarning>
       <body className={cn(font.className)} suppressHydrationWarning>
         {/* <div className="container py-2 h-fit md:py-18 grow">{children}</div> */}
-
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   );
