ec20c8d76ca28b45989116ce3183f1493a20802c
hyunjin <leehj0110@kakao.com>
2023-07-30 15:50:06 +0900

    display songs

--- apps/practice/app/(site)/components/PageContent.tsx
+++ apps/practice/app/(site)/components/PageContent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import SongItem from '@/components/SongItem';
 import { Song } from '@/types';
 
 interface PageContentProps {
@@ -12,7 +13,9 @@ const PageContent = ({ songs }: PageContentProps) => {
   }
   return (
     <div className="grid grid-cols-2 gap-4 mt-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8">
-      PageContent
+      {songs.map((song) => {
+        return <SongItem data={song} onClick={() => {}} />;
+      })}
     </div>
   );
 };
---
+++ apps/practice/components/SongItem.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import useLoadImage from '@/hooks/useLoadImage';
+import { Song } from '@/types';
+import Image from 'next/image';
+import React from 'react';
+import PlayButton from './PlayButton';
+
+type SongItemProps = {
+  data: Song;
+  onClick: (id: string) => void;
+};
+
+const SongItem = ({ data, onClick }: SongItemProps) => {
+  console.log(data);
+  const imagePath = useLoadImage(data);
+
+  return (
+    <div
+      onClick={() => {
+        onClick(data.id);
+      }}
+      className="relative flex flex-col items-center justify-center p-3 overflow-hidden transition rounded-md cursor-pointer group bg-neutral-400/5 gap-x-4 hover:bg-neutral-400/10"
+    >
+      <div className="relative w-full h-full overflow-hidden rounded-md aspect-square">
+        <Image className="object-cover" src={imagePath || '/images/liked.png'} alt="image" fill />
+      </div>
+      <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+        <p className="w-full font-semibold truncate">{data.title}</p>
+        <p className="w-full pb-4 text-sm truncate text-neutral-400">By {data.author}</p>
+      </div>
+      <div className="absolute bottom-24 right-5">
+        <PlayButton />
+      </div>
+    </div>
+  );
+};
+
+export default SongItem;
