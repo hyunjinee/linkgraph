f3746a22f19c4d9c23fd1d87ae2237724f2b4869
hyunjin <leehj0110@kakao.com>
2023-08-01 14:25:13 +0900

    refactor: Search Component (open close 로직)

--- apps/service/src/components/Search.tsx
+++ apps/service/src/components/Search.tsx
@@ -1,41 +1,44 @@
 'use client';
 
+import Link from 'next/link';
 import Image from 'next/image';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { SearchIcon, X } from 'lucide-react';
 import { useQuery } from '@tanstack/react-query';
 
+import type { User } from '.prisma/client';
 import Spinner from './Spinner';
 import { useDebounce } from '~/hooks/useDebounce';
-import type { User } from '.prisma/client';
 import { cn } from '~/lib/utils';
 import { useOnClickOutside } from '~/hooks/useOnClickOutside';
-import Link from 'next/link';
 
 const Search = () => {
   const [keyword, setKeyword] = useState('');
   const debouncedKeyword = useDebounce(keyword);
   const searchParams = new URLSearchParams({ keyword: debouncedKeyword });
   const searchContainerRef = useRef<HTMLDivElement>(null);
-  const [showResult, setShowResult] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const { data: users, isFetching } = useQuery<User[]>({
     queryKey: ['search', searchParams.get('keyword')],
-    queryFn: async () => {
-      return fetch(`/api/search?${searchParams.toString()}`).then((res) => res.json());
-    },
+    queryFn: async () => fetch(`/api/search?${searchParams.toString()}`).then((res) => res.json()),
     enabled: debouncedKeyword.length > 0,
-    onSuccess: () => setShowResult(true),
   });
 
-  useOnClickOutside(searchContainerRef, () => setShowResult(false));
+  useOnClickOutside(searchContainerRef, () => setOpen(false));
+
+  useEffect(() => {
+    if (users && users.length > 0) {
+      setOpen(true);
+    }
+  }, [users]);
 
   return (
     <div
       ref={searchContainerRef}
       className={cn(
         'sm:w-[360px] h-[44px] w-full rounded-lg bg-[#fafafa] relative',
-        users && users.length > 0 && 'rounded-br-none rounded-bl-none',
+        open && 'rounded-br-none rounded-bl-none',
       )}
     >
       <div className="flex items-center justify-center h-full p-3 ">
@@ -53,7 +56,7 @@ const Search = () => {
           <X size={25} className="transition cursor-pointer hover:text-pink-300" onClick={() => setKeyword('')} />
         )}
       </div>
-      {showResult && users && users.length > 0 && (
+      {open && (
         <ul className="absolute w-full z-50 bg-[#fafafa] flex flex-col gap-4 p-4 top-[44px] rounded-bl-lg rounded-br-lg">
           {users?.map((user) => (
             <Link key={user.id} href={user.url || user.id} className="cursor-pointer">
