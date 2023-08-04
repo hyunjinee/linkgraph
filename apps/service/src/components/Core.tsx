5f329d5cfbddfce5d6e418747ad8a4ba2f765d6f
hyunjin <leehj0110@kakao.com>
2023-08-04 12:50:10 +0900

    what is wrong

--- apps/service/src/components/Core.tsx
+++ apps/service/src/components/Core.tsx
@@ -2,7 +2,6 @@
 
 import React, { type PropsWithChildren } from 'react';
 import type { Session } from 'next-auth';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
