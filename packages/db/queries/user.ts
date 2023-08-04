ce3c5afa3cb4f8921ac81dabc7ea5342a262d6fa
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2023-08-04 10:13:57 +0900

    feat: Type UserWithLinks (#93)
    * docs: update readme
    
    * type UserWithLinks with prisma

--- packages/db/queries/user.ts
+++ packages/db/queries/user.ts
@@ -1,4 +1,12 @@
-export const getRandomUserWithLinks = async () => {
+import { Prisma } from '@prisma/client';
+
+type UserWithLinks = Prisma.UserGetPayload<{
+  include: {
+    links: true;
+  };
+}>;
+
+export const getRandomUserWithLinks = async (): Promise<UserWithLinks> => {
   const totalRecords = await prisma.user.count();
   const randomIndex = Math.floor(Math.random() * totalRecords);
 
@@ -9,5 +17,9 @@ export const getRandomUserWithLinks = async () => {
     },
   });
 
-  return randomUser;
+  if (randomUser && randomUser.links.length >= 3) {
+    return randomUser;
+  }
+
+  return getRandomUserWithLinks();
 };
